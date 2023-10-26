0582a09853313ee5ad34485949c2ac31ef2d7125
Sourav Das <souravdasdip65@gmail.com>
2023-10-27 01:24:01 +0600

    categories section modification

--- components/TopCategory.tsx
+++ components/TopCategory.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const TopCategory = () => {
@@ -15,37 +16,37 @@ const TopCategory = () => {
       color: "#E1F7E3",
     },
     {
-      label: "Label",
+      label: "Development",
       sublabel: "63,476 Courses",
       icon: "label",
-      color: "#EBEBFF",
+      color: "#E1F7E3",
+    },
+    {
+      label: "Lifestyle ",
+      sublabel: "63,476 Courses",
+      icon: "business",
+      color: "#F5F7FA",
+    },
+    {
+      label: "Design",
+      sublabel: "63,476 Courses",
+      icon: "label",
+      color: "#F5F7FA",
     },
     {
       label: "Business",
       sublabel: "63,476 Courses",
       icon: "business",
-      color: "#E1F7E3",
+      color: "#FFF2E5 ",
     },
     {
-      label: "Label",
+      label: "Office Productivity ",
       sublabel: "63,476 Courses",
       icon: "label",
       color: "#EBEBFF",
     },
     {
-      label: "Business",
-      sublabel: "63,476 Courses",
-      icon: "business",
-      color: "#E1F7E3",
-    },
-    {
-      label: "Label",
-      sublabel: "63,476 Courses",
-      icon: "label",
-      color: "#EBEBFF",
-    },
-    {
-      label: "Business",
+      label: "Marketing",
       sublabel: "63,476 Courses",
       icon: "business",
       color: "#E1F7E3",
@@ -61,8 +62,10 @@ const TopCategory = () => {
             className={styles.cat}
             style={{ backgroundColor: `${cat.color}` }}
           >
-            <span className={styles.category_icon}>{cat.icon}</span>
-            <div>
+            <span className={styles.category_icon}>
+              <Image width={40} height={40} src={"/icon.png"} alt="icon" />{" "}
+            </span>
+            <div style={{ flex: 0.6 }}>
               <p className={styles.cat_title}>{cat.label}</p>
               <p className={styles.cat_subtitle}>{cat.sublabel}</p>
             </div>
